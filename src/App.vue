eb1d68cba844327281e1b62fd02ead047504a668
mao-118 <383397258@qq.com>
2022-08-03 15:10:57 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
 </template>
 <script setup lang="ts">
 import Loading from "./components/Loading.vue";
-console.log(import.meta.env);
-console.log(import.meta.env.VITE_BASE_URL);
 </script>
 <style lang="scss" scoped>
 </style>
