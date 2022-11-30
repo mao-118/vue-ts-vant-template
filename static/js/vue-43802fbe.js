ba0966b126276f530c9b5491fdbb330d38b817ce
mao-118 <>
2022-11-30 03:41:28 +0000

    Deploying to gh-pages from @ mao-118/vue-ts-vant-template@981a7a6e7252804c4a06ec6caebcbe89e83984db 🚀

--- static/js/vue-43802fbe.js
+++
@@ -1,1 +0,0 @@
-import{x as a}from"./@vue-6a45e322.js";a();
--- static/js/vue-legacy-f3724d66.js
+++
@@ -1,1 +0,0 @@
-System.register(["./@vue-legacy-5903f947.js"],(function(){"use strict";var e;return{setters:[function(t){e=t.x}],execute:function(){e()}}}));
