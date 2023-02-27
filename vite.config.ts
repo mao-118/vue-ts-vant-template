4f708f1c27457fd33ad3d5e9dee8f5a2deb4445e
mao-118 <383397258@qq.com>
2023-02-27 17:28:27 +0800

    feat：修改b rbrowserslist

--- vite.config.ts
+++ vite.config.ts
@@ -78,16 +78,7 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        autoprefixer({
-          overrideBrowserslist: [
-            'Android 4.1',
-            'iOS 7.1',
-            'Chrome > 31',
-            'ff > 31',
-            'ie >= 8',
-            // 'last 2 versions', // 所有主流浏览器最近2个版本
-          ],
-        }),
+        autoprefixer(),
         tailwindcss(),
         PostcssPxToViewport({
           unitToConvert: 'px', // 要转化的单位
