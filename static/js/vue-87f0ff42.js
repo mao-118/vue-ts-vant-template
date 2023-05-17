01b5a0e5a63ad92baa244dec701423a36cd7243d
mao-118 <>
2023-05-17 07:32:05 +0000

    Deploying to gh-pages from @ mao-118/vue-ts-vant-template@a1ff01d2c6b2f19d4b2b59a349878c926e34d932 🚀

--- static/js/vue-87f0ff42.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./@vue-ee838e4f.js";e();
--- static/js/vue-legacy-a8f8caf9.js
+++
@@ -1,1 +0,0 @@
-System.register(["./@vue-legacy-8e6dcd26.js"],(function(e,t){"use strict";var c;return{setters:[function(e){c=e.x}],execute:function(){c()}}}));
